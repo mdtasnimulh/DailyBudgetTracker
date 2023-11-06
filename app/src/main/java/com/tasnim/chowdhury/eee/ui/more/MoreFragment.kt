e4a8c7f2e07bc50fb3d3bccbc2295089252fc833
Md. Tasnimul Hasan <mdtasnimulh@gmail.com>
2023-11-07 02:21:35 +0600

    inside calendar view perday income and expense added with some functionality

--- app/src/main/java/com/tasnim/chowdhury/eee/ui/more/MoreFragment.kt
+++ app/src/main/java/com/tasnim/chowdhury/eee/ui/more/MoreFragment.kt
@@ -6,15 +6,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tasnim.chowdhury.eee.R
+import com.tasnim.chowdhury.eee.databinding.FragmentMoreBinding
 
 class MoreFragment : Fragment() {
+
+    private lateinit var binding: FragmentMoreBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false)
+        binding = FragmentMoreBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initData()
+        setupClicks()
+    }
+
+    private fun initData() {
+
+    }
+
+    private fun setupClicks() {
+
     }
 
 }
