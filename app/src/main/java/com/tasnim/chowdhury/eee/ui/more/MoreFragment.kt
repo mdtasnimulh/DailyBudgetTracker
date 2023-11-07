247a01b0ad97b8d1f59cc7f70366d2887f273152
tasnimul.hasan <mdtasnimulh@gmail.com>
2023-11-07 16:30:26 +0600

    adapter item icon and background fixed.

--- app/src/main/java/com/tasnim/chowdhury/eee/ui/more/MoreFragment.kt
+++ app/src/main/java/com/tasnim/chowdhury/eee/ui/more/MoreFragment.kt
@@ -1,16 +1,41 @@
 package com.tasnim.chowdhury.eee.ui.more
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.tasnim.chowdhury.eee.R
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.preference.PreferenceManager
 import com.tasnim.chowdhury.eee.databinding.FragmentMoreBinding
 
 class MoreFragment : Fragment() {
 
     private lateinit var binding: FragmentMoreBinding
+    private lateinit var sharedPreferences: SharedPreferences
+
+    private val changeImage = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if (it.resultCode == Activity.RESULT_OK) {
+            val data = it.data
+            val imageUri = data?.data
+            binding.coverImage.setImageURI(imageUri)
+
+            // Save the selected image URI in SharedPreferences
+            saveImageUri(imageUri)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +44,12 @@ class MoreFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentMoreBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -30,10 +61,35 @@ class MoreFragment : Fragment() {
 
     private fun initData() {
 
+
     }
 
     private fun setupClicks() {
+        binding.coverEditIcon.setOnClickListener {
+            val pickImg = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            changeImage.launch(pickImg)
+        }
 
+        // Load the saved image URI when the fragment is created
+        val savedImageUri = loadImageUri()
+        if (savedImageUri != null) {
+            binding.coverImage.setImageURI(savedImageUri)
+        }
+    }
+
+    private fun saveImageUri(uri: Uri?){
+        val editor = sharedPreferences.edit()
+        editor.putString("cover_image_uri", uri.toString())
+        editor.apply()
+    }
+
+    private fun loadImageUri(): Uri? {
+        val uriString = sharedPreferences.getString("cover_image_uri", null)
+        return if (!uriString.isNullOrBlank()){
+            Uri.parse(uriString)
+        }else {
+            null
+        }
     }
 
 }
